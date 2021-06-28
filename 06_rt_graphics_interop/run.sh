5f1777ee0cd54f5bd09357d8b1c6c0384c543e74
Nyriu <muninitristano@gmail.com>
2021-06-28 19:00:55 +0200

    Main classes

--- 06_rt_graphics_interop/run.sh
+++ 06_rt_graphics_interop/run.sh
@@ -5,21 +5,16 @@ if [ "$1" == "-b" ]; then
 fi
 
 if [ -d "./build" ]; then
+  rm -rf main
   cd build
   make
   cd ..
-fi
-
-if [ ! -f "./main" ]; then
+elif [ ! -f "./main" ]; then
   echo "Building..."
   rm -rf build main
   mkdir build
   cd build
   cmake -D CMAKE_CUDA_COMPILER=/opt/cuda/bin/nvcc ..
-  #cmake -D CUDAToolkit_ROOT=/opt/cuda/ -D CMAKE_CUDA_COMPILER=/opt/cuda/bin/nvcc ..
-  #cmake -DCMAKE_CUDA_FLAGS=”-arch=sm_50” ..
-  #cmake -DCMAKE_CUDA_FLAGS=”-gencode arch=compute_50,code=sm_50” ..
-
   make
   cd ..
 fi
--- 06_rt_graphics_interop/src/main.cu
+++ 06_rt_graphics_interop/src/main.cu
@@ -1,14 +1,20 @@
+#include <glm/common.hpp>
 #define GL_GLEXT_PROTOTYPES
 
 #include <iostream>
-#include <stdlib.h>
-
-#ifdef __clang__
-#include <__clang_cuda_math.h>
-#include <__clang_cuda_builtin_vars.h>
-#endif
+//#include <stdlib.h>
+//
+//#ifdef __clang__
+//#include <__clang_cuda_math.h>
+//#include <__clang_cuda_builtin_vars.h>
+//#endif
 
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+//#include <glm/vec3.hpp>
+#include <glm/fwd.hpp>
+#include <glm/geometric.hpp>
+
 #include <cuda.h>
 #include <cuda_gl_interop.h>
 
@@ -29,34 +35,205 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 
 
+// Aliases
+using vec3   = glm::vec3;
+using point3 = glm::vec3;
+using color  = glm::vec3;
+
 // Globals
-#define DIM 512
+#define IMG_H 512/4
+#define IMG_W 512/4
 
-GLuint bufferObj;
-cudaGraphicsResource *resource;
+//#define IMG_H 1080
+//#define IMG_W IMG_H*16/9;
 
 
-__global__ void kernel(uchar4 *ptr) {
+class Sphere {
+  private:
+    float radius_;
+    point3 center_;
+
+  public:
+    Sphere(const float& radius) : radius_(radius) {
+      center_ = point3(0);
+    }
+
+    float getDist(const point3& point) const {
+      return glm::length(point) - radius_;
+    }
+};
+
+class Ray {
+  public:
+    const point3 orig_;
+    const vec3 dir_;
+
+    __device__ Ray() : orig_(point3(0)), dir_(glm::normalize(vec3(0,0,-1))) { }
+    __device__ Ray(const point3& origin, const vec3& direction) :
+      orig_(origin), dir_(glm::normalize(direction)) {}
+
+    const point3 at(float t) const {
+      return orig_ + t*dir_;
+    }
+};
+
+class Camera {
+  private:
+    point3 center_;
+    vec3 dir_;
+
+    float aspect_;
+    float fov_;
+
+  public:
+    Camera() {
+      center_ = point3(0,0,5);
+      dir_ = glm::normalize(vec3(0,0,-1));
+      aspect_ = 1;
+      fov_ = 45;
+    }
+
+    __device__ Ray generate_ray(float u, float v) const { // input NDC Coords
+      // Put coords in [-1,1]
+      float su = 2 * u - 1; // Screen Coord
+      float sv = 1 - 2 * v; // Screen Coord (flip y axis)
+
+      // Aspect Ratio
+      su *= aspect_; // x in [-asp ratio, asp ratio]
+      // y in [-1,1] (as before)
+
+      // Field Of View
+      su *= std::tan(fov_/2);
+      sv *= std::tan(fov_/2);
+
+      //float scale = 1;
+      //// Scale
+      //su *= scale;
+      //sv *= scale;
+
+      // From ScreenCoords to WorldCoords
+      point3 p    = point3(su,sv,center_.z - 1) ;
+      point3 orig = center_;
+      return Ray(orig, glm::normalize((orig - p)));
+    }
+};
+
+
+class Tracer {
+  public:
+    __device__ color trace(const Ray *r, const Sphere *sph) const {
+      //TODO sphere tracing
+      return color((float).25);
+    };
+  private:
+    // TODO
+    //  Color sphereTrace(const Ray& r); // better with pointer?
+    //  Color shade(const Point3& p, const Vec3& viewDir, const ImplicitShape *shape);
+    //  bool sphereTraceShadow(const Ray& r, const ImplicitShape *shapeToShadow);
+};
+
+static __global__ void kernel(uchar4 *ptr,
+    const Camera *cam,
+    const Sphere *sph,
+    const Tracer *trc
+    ) {
+
   // map from threadIdx/BlockIdx to pixel position
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
   int offset = x + y * blockDim.x * gridDim.x;
 
-  // now calculate the value at that position
-  float fx = x/(float)DIM - 0.5f;
-  float fy = y/(float)DIM - 0.5f;
-  unsigned char green =
-    128 + 127 * sin(abs(fx*100) - abs(fy*100));
+  //// now calculate the value at that position
+  //float fx = x/(float)IMG_W - 0.5f;
+  //float fy = y/(float)IMG_H - 0.5f;
+  //unsigned char green =
+  //  128 + 127 * sin(abs(fx*100) - abs(fy*100));
+
+  // in img coord (0,0) is top-left
+  // Put coords in [0,1]
+  float u = (x + .5) / ((float) IMG_W -1); // NDC Coord
+  float v = (y + .5) / ((float) IMG_H -1); // NDC Coord
+  Ray r = cam->generate_ray(u,v);
+
+  color c = trc->trace(&r, sph);
+
+  //img[j*IMG_W+i + 0] = (int) 255 * c.r;
+  //img[j*IMG_W+i + 1] = (int) 255 * c.g;
+  //img[j*IMG_W+i + 2] = (int) 255 * c.b;
+  //img[j*IMG_W+i + 3] = (int) 255 * 1;
+  //printf("(x,y) = (%d, %d)\n", x,y);
+  //printf("(u,v) = (%f, %f)\n", u,v);
+  //printf("[%f, %f, %f]\n", c.r, c.g, c.b);
+  //printf("[%d, %d, %d]\n", (int)(255*c.r), (int)(255*c.g), (int)(255*c.b));
 
   // accessing uchar4 vs unsigned char*
-  ptr[offset].x = 0;
-  ptr[offset].y = green;
-  ptr[offset].z = 0;
+  ptr[offset].x = (int) (255 * c.r); // (int) (u * 255); //0;
+  ptr[offset].y = (int) (255 * c.g); // (int) (v * 255); //(int)255/2;
+  ptr[offset].z = (int) (255 * c.b); // 0;
   ptr[offset].w = 255;
 }
+class Renderer {
+  private:
+    Tracer *tracer_;
+  public:
+    void render(
+        const Camera *cam,
+        const Sphere *sph,
+              uchar4 *devPtr) {
+      // --- Generate One Frame ---
+
+      // TODO qui va il kernel al posto dei for
+      dim3 grids(IMG_W/16, IMG_H/16);
+      dim3 threads(16,16);
+      kernel<<<grids,threads>>>(devPtr, cam, sph, tracer_);
+
+      //// in img coord (0,0) is top-left
+      //for (int j=0; j<IMG_H; ++j) {
+      //  for (int i=0; i<IMG_W; ++i) {
+      //    // Put coords in [0,1]
+      //    float u = double(i + .5) / (IMG_W -1); // NDC Coord
+      //    float v = double(j + .5) / (IMG_H -1); // NDC Coord
+      //    Ray r = cam->generate_ray(u,v);
+
+      //    color c = tracer_->trace(r);
+
+      //    img[j*IMG_W+i + 0] = (int) 255 * c.r;
+      //    img[j*IMG_W+i + 1] = (int) 255 * c.g;
+      //    img[j*IMG_W+i + 2] = (int) 255 * c.b;
+      //    img[j*IMG_W+i + 3] = (int) 255 * 1;
+      //  }
+      //}
+    }
+  private:
+    // TODO far funzionare qua
+    //static __global__ void kernel(uchar4 *ptr) {
+    //  // map from threadIdx/BlockIdx to pixel position
+    //  int x = threadIdx.x + blockIdx.x * blockDim.x;
+    //  int y = threadIdx.y + blockIdx.y * blockDim.y;
+    //  int offset = x + y * blockDim.x * gridDim.x;
+
+    //  // now calculate the value at that position
+    //  float fx = x/(float)IMG_W - 0.5f;
+    //  float fy = y/(float)IMG_H - 0.5f;
+    //  unsigned char green =
+    //    128 + 127 * sin(abs(fx*100) - abs(fy*100));
+
+    //  // accessing uchar4 vs unsigned char*
+    //  ptr[offset].x = 0;
+    //  ptr[offset].y = green;
+    //  ptr[offset].z = 0;
+    //  ptr[offset].w = 255;
+    //}
+};
 
 
-int main(int argc, char **argv) {
+
+GLuint bufferObj;
+cudaGraphicsResource *resource;
+
+
+
+int main() {
   cudaDeviceProp prop;
   int dev;
 
@@ -66,10 +243,6 @@ int main(int argc, char **argv) {
   HANDLE_ERROR(
       cudaChooseDevice(&dev,&prop)
       );
-  //HANDLE_ERROR(
-  //    cudaGLSetGLDevice(dev) // deprecated
-  //    );
-
 
 	if (!glfwInit()) exit(EXIT_FAILURE);
 	if (atexit(glfwTerminate)) {
@@ -78,7 +251,7 @@ int main(int argc, char **argv) {
 	}
 
 	GLFWwindow* window;
-	window = glfwCreateWindow(DIM, DIM, "GLFW Window", NULL, NULL);
+	window = glfwCreateWindow(IMG_W, IMG_H, "GLFW Window", NULL, NULL);
 	if (!window) exit(EXIT_FAILURE);
 
   glfwSetKeyCallback(window, key_callback);
@@ -96,7 +269,7 @@ int main(int argc, char **argv) {
 	//glBufferData(GL_PIXEL_UNPACK_BUFFER, 4 * sizeof(GLubyte)*WIDTH*HEIGHT, NULL, GL_DYNAMIC_DRAW);
   glGenBuffers(1, &bufferObj);
   glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, bufferObj);
-  glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB, DIM*DIM*4, NULL, GL_DYNAMIC_DRAW_ARB);
+  glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB, IMG_H*IMG_W*4, NULL, GL_DYNAMIC_DRAW_ARB);
 
   HANDLE_ERROR(
       cudaGraphicsGLRegisterBuffer(
@@ -119,17 +292,21 @@ int main(int argc, char **argv) {
         )
       );
 
-  dim3 grids(DIM/16, DIM/16);
-  dim3 threads(16,16);
-  kernel<<<grids,threads>>>(devPtr);
+  Camera cam;
+  Sphere sph(1);
+  Renderer renderer;
+  renderer.render(&cam, &sph, devPtr);
+
+  
 
   HANDLE_ERROR(
       cudaGraphicsUnmapResources(1, &resource, NULL)
       );
 
+
 	while (!glfwWindowShouldClose(window)) {
 		//kernelUpdate(WIDTH, HEIGHT);
-		glDrawPixels(DIM, DIM, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+		glDrawPixels(IMG_W, IMG_H, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 		glfwSwapBuffers(window);
 
     // Poll for and process events
